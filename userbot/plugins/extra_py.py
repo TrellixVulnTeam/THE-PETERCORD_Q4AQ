3c18c3889f5148589c54863fde10a5ddd80f92a2
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-27 22:45:37 +0700

    Update extra_py.py

--- userbot/plugins/extra_py.py
+++ userbot/plugins/extra_py.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from telethon.tl.types import InputMessagesFilterDocument
 
-from REBELBOT.utils import admin_cmd, load_module
+from PETERCORDBOT.utils import admin_cmd, load_module
 from var import Var
 from userbot.cmdhelp import CmdHelp
 
