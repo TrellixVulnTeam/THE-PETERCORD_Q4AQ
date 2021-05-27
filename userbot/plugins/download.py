a63f4af3ca3220518dbe09ebd24943a983fbf54d
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-27 22:05:01 +0700

    Update download.py

--- userbot/plugins/download.py
+++ userbot/plugins/download.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from pySmartDL import SmartDL
 
 from userbot import ALIVE_NAME, CMD_HELP
-from PETWRCORDBOT.utils import admin_cmd, humanbytes, progress, sudo_cmd
+from PETERCORDBOT.utils import admin_cmd, humanbytes, progress, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "PETERCORD User"
