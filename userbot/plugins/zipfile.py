8dd53dfaa34f2c765b5601ce83791c786559ed4a
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-06 12:11:32 +0700

    Update zipfile.py

--- userbot/plugins/zipfile.py
+++ userbot/plugins/zipfile.py
@@ -45,7 +45,7 @@ async def _(event):
             await borg.send_file(
                 event.chat_id,
                 directory_name + ".zip",
-                caption="Zipped By [REBELBOT](t.me/REBELBOT_Support)",
+                caption="Zipped By Ilham mansiez",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
