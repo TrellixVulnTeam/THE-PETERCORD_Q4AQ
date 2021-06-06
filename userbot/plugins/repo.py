024a48a1d472eeaca4c6db38931a98a859ed1208
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-06 12:14:47 +0700

    Create repo.py

---
+++ userbot/plugins/repo.py
@@ -0,0 +1,26 @@
+
+import asyncio
+from collections import deque
+
+from telethon.tl.functions.users import GetFullUserRequest
+
+from userbot import *
+from PETERCORDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.cmdhelp import CmdHelp
+
+
+@bot.on(admin_cmd(pattern=f"repo$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"repo$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.1
+    animation_ttl = range(0, 36)
+    await edit_or_reply(event, "REPO PETERCORD")
+    animation_chars = [
+        "REPO PETERCORD",
+        "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n [𝗣 𝗘 𝗧 𝗘 𝗥 𝗖 𝗢 𝗥 𝗗](https://github.com/ilham77mansiz/-PETERCORD-)\n\n [𝗢 𝗪 𝗡 𝗘 𝗥 𝗦](t.me/diemmmmmmmmmm)\n\n [GRUP SUPPORT](https://t.me/TEAMSquadUserbotSupport)\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 18])
