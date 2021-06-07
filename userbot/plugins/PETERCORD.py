de41df5f53e1ff7d40f444abbb8abeb89353fbf1
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-07 18:21:54 +0700

    Update PETERCORD.py

--- userbot/plugins/PETERCORD.py
+++ userbot/plugins/PETERCORD.py
@@ -83,4 +83,23 @@ async def amireallyalive(alive):
             f"**★ 𝗨𝗣𝗧𝗜𝗠𝗘 :** `{uptime}\n`"
             f"**★ 𝗠𝗔𝗦𝗧𝗘𝗥 :** {mention}\n",
         )
-    
+        ("@TEAMSquadUserbotSupport",
+                text="""**Hey! This is [PETERCORDBOT.](https://t.me/TEAMSquadUserbotSupport) \nYou can know more about me from the links given below 👇**""",
+                buttons=[
+                    [
+                        custom.Button.url("🔥 CHANNEL 🔥", "https://t.me/TEAMSquadUserbotSupport"),
+                        custom.Button.url(
+                            "⚡ GROUP ⚡", "https://t.me/TEAMSquadUserbotSupport"
+                        ),
+                    ],
+                    [
+                        custom.Button.url(
+                            "✨ REPO ✨", "https://github.com/IlhamMansiez/PETERCORDBOT"),
+                        custom.Button.url
+                    (
+                            "🔰 TUTORIAL 🔰", ""
+                    )
+                    ],
+                ],
+                link_preview=False,
+            )
