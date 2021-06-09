0bf690d7ff9a5d63d5ff8b35eedd2f008df256c5
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-09 19:06:42 +0700

    Update repo.py

--- userbot/plugins/repo.py
+++ userbot/plugins/repo.py
@@ -8,18 +8,10 @@ from userbot import *
 from PETERCORDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
+@bot.on(admin_cmd(pattern=f"repo", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"repo", allow_sudo=True))
+async def source(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await edit_or_reply(e, "╭┄┅┯┅┄┄┅┯┅┄╮**\n\n [𝗣 𝗘 𝗧 𝗘 𝗥 𝗖 𝗢 𝗥 𝗗](https://github.com/IlhamMansiez/PETERCORDBOT)\n\n [𝗢 𝗪 𝗡 𝗘 𝗥 𝗦](t.me/diemmmmmmmmmm)\n\n [GRUP SUPPORT](https://t.me/TEAMSquadUserbotSupport)\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n")
 
-@bot.on(admin_cmd(pattern=f"repo$", outgoing=True))
-@bot.on(sudo_cmd(pattern=f"repo$", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    animation_interval = 0.1
-    animation_ttl = range(0, 36)
-    await edit_or_reply(event, "REPO PETERCORD\n\n**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n [𝗣 𝗘 𝗧 𝗘 𝗥 𝗖 𝗢 𝗥 𝗗](https://github.com/IlhamMansiez/PETERCORDBOT)\n\n [𝗢 𝗪 𝗡 𝗘 𝗥 𝗦](t.me/diemmmmmmmmmm)\n\n [GRUP SUPPORT](https://t.me/TEAMSquadUserbotSupport)\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**")
-    animation_chars = [
-        "**╭┄┅┯┅┄┄┅┯┅┄╮**\n\n [𝗣 𝗘 𝗧 𝗘 𝗥 𝗖 𝗢 𝗥 𝗗](https://github.com/IlhamMansiez/PETERCORDBOT)\n\n [𝗢 𝗪 𝗡 𝗘 𝗥 𝗦](t.me/diemmmmmmmmmm)\n\n [GRUP SUPPORT](https://t.me/TEAMSquadUserbotSupport)\n\n**╰┄┅┷┅┄┄┅┷┅┄╯**\n",
-    ]
-    for i in animation_ttl:
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 18])
+
