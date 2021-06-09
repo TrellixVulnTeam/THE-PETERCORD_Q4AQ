974328831cff0656f7de8c4dbe7566573409beee
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-09 17:34:36 +0700

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -619,12 +619,6 @@ async def vapor(vpr):
         await edit_or_reply(vpr, "".join(reply_text))
 
 
-@bot.on(admin_cmd(pattern=f"repo", outgoing=True))
-@bot.on(sudo_cmd(pattern=f"repo", allow_sudo=True))
-async def source(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await edit_or_reply(e, "Click [here](https://github.com/REBEL725/REBELBOT) to open this 🔥**Lit AF!!**🔥 __REBELBOT__ Repo.. Join channel :- @REBELBOT_Support")
-
 
 @bot.on(admin_cmd(pattern="str(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="str(?: |$)(.*)", allow_sudo=True))
@@ -874,4 +868,4 @@ CmdHelp("memes").add_command(
 ).add_command(
   "cowsay", "<text>", "Use and see....."
 ).add()
-#REBELBOT_OP
+#PetercordBot
