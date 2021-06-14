5802f121b5902947a6257afcc4ac168e4d30bfdc
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-14 12:58:51 +0700

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -120,6 +120,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def on_plug_in_callback_query_handler(event):
+    @check_owner
     buttons = [
         (Button.inline("Open Menu", data="mainmenu"),),
     ]
