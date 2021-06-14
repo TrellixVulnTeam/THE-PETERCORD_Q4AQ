1fde0c94d2c89cdd91b113bca84315d5f9f61a35
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-14 13:37:07 +0700

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -121,7 +121,6 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def page(event):
         if not event.query.user_id == bot.uid:
-    try:
         buttons = [
             custom.Button.inline(("Open Menu", data="button"),),
     ]
